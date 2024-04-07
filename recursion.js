012dea88c496c01c4bafc073c8b2651b1642f83c
Samuel <samuelrivba@gmail.com>
2024-04-07 17:57:22 -0500

    Add factorial implementations

--- recursion.js
+++ recursion.js
@@ -1,3 +1,7 @@
+// 1. Identify the base case
+// 2. Identify the recursive case
+// 3. Get closer and closer and return when needed. Usually you have 2 returnslet counter = 0;
+
 let counter = 0;
 function inception() {
   console.log(counter);
@@ -10,6 +14,27 @@ function inception() {
 
 console.log(inception());
 
-// 1. Identify the base case
-// 2. Identify the recursive case
-// 3. Get closer and closer and return when needed. Usually you have 2 returns
+// Factorial
+
+function factorialRecursive(num) {
+  // O(n)
+  if (num <= 2) {
+    return num;
+  }
+  return num * factorialRecursive(num - 1);
+}
+
+function factorialIterative(num) {
+  // O(n)
+  let fact = 1;
+  if (num === 2) {
+    return (fact = 2);
+  }
+  for (let i = 2; i <= num; i++) {
+    fact *= i;
+  }
+  console.log(fact);
+}
+
+console.log(factorialRecursive(5));
+factorialIterative(5);
